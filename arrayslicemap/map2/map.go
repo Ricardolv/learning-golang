92275c96718327feb45404896a1763414e826515
Ricardo Viana <richardluizv@gmail.com>
2020-07-04 08:57:48 -0300

    trabalhando com map de outra forma

---
+++ arrayslicemap/map2/map.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	funcsESalarios := map[string]float64{
+		"Liliane":  11325.45,
+		"Bernardo": 15456.78,
+		"Maressa":  1200.0,
+	}
+
+	fmt.Println(funcsESalarios)
+
+	funcsESalarios["Ricardo"] = 1350.0
+
+	fmt.Println(funcsESalarios)
+	delete(funcsESalarios, "Inexistente")
+
+	for nome, salario := range funcsESalarios {
+		fmt.Println(nome, salario)
+	}
+
+}
